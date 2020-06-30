73b1d4d8decc0d900b5b917066fffd6dccd3f39a
german.reyga@gmail.com <german.reyga@gmail.com>
2020-06-30 13:18:11 -0500

    Build changed to Docs

---
+++ docs/precache-manifest.9b9608290db34713444b3841078c9493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acba31550d952330839e7c847358745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55bc414714d05c50c6b3",
+    "url": "/static/css/main.b0579bd0.chunk.css"
+  },
+  {
+    "revision": "ce34e024298a29138825",
+    "url": "/static/js/2.87d7dfe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87d7dfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55bc414714d05c50c6b3",
+    "url": "/static/js/main.8fd69094.chunk.js"
+  },
+  {
+    "revision": "fcbd203b8aa8276edfa9",
+    "url": "/static/js/runtime-main.059747aa.js"
+  }
+]);
